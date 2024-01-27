f404ff7eead95fb9173f8a5b27d4da900f0244f0
David-63 <jiseop.63@gmail.com>
2024-01-27 16:39:32 +0900

    skybox, light UI에서 타입 선택을 enum으로 변경

--- External/Include/Engine/CComponent.h
+++ External/Include/Engine/CComponent.h
@@ -36,9 +36,11 @@ public:
     GET_OTHER_COMPONENT(Camera);
     GET_OTHER_COMPONENT(Collider2D);
     GET_OTHER_COMPONENT(Light2D);
+    GET_OTHER_COMPONENT(Light3D);
     GET_OTHER_COMPONENT(TileMap);
     GET_OTHER_COMPONENT(Animator2D);
     GET_OTHER_COMPONENT(Animator3D);
+    GET_OTHER_COMPONENT(LandScape);
 
 public:
     CComponent(COMPONENT_TYPE _Type);
--- External/Include/Engine/CSkyBox.h
+++ External/Include/Engine/CSkyBox.h
@@ -6,6 +6,7 @@ enum class SKYBOX_TYPE
 {
     SPHERE,
     CUBE,
+    End,
 };
 
 class CSkyBox :
